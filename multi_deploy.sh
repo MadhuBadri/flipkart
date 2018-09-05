580f7bcae5ce9d3f23462aceddd6c6e675cc244e
MadhuBadri <muddulurumadhu007@gmail.com>
2018-09-05 11:57:18 +0530

    Added the javapath

--- multi_deploy.sh
+++ multi_deploy.sh
@@ -8,7 +8,7 @@ if [ $ENVIRONMENT = "QA" ]; then
 sshpass -p "madhu" scp target/MavenWebProject.war Madhu@172.17.0.2:/home/softwares/apache-tomcat-8.5.33/webapps
          echo "Starting tomcat server in $i QA server"
 	 sleep 3
-sshpass -p "madhu" ssh Madhu@172.17.0.2 "JAVA_HOME=/home/softwares/jdk1.8.0_181"/home/softwares/apache-tomcat-8.5.33/bin/startup.sh
+sshpass -p "madhu" ssh Madhu@172.17.0.2 "JAVA_HOME=/home/softwares/jdk1.8.0_181" /home/softwares/apache-tomcat-8.5.33/bin/startup.sh
    done
    echo "Deployment is successfu"
    fi
