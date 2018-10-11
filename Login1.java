11c2f6cdc0b59a2d035fa60a4b8722b19db6a339
MadhuBadri <muddulurumadhu007@gmail.com>
2018-10-11 16:26:47 +0530

    Brought the recent commit to 3 changes before

---
+++ Login1.java
@@ -0,0 +1,13 @@
+public class Login1
+{
+	public static void madin()
+		 {
+	 system.out.println("Hi How are you");
+	 system.out.println("what are u doing now");
+<<<<<<< HEAD
+	 system.out.println("I am learning Oracle,,,");
+=======
+	 system.out.println("I am learning shell,,,");
+>>>>>>> dev_1.2.3
+		 }
+}
---
+++ payment.java
@@ -0,0 +1,9 @@
+public class Login1
+{
+	public static void madin()
+		 {
+	 system.out.println("Hi How are you");
+	 system.out.println("what are u doing now");  
+         system.out.println("Hi Madhu");	 
+		 }
+}
